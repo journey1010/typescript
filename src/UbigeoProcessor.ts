e9efebb5a94ef8b4df2a0d4c472cf519c0bcb373
other@gmail <other@gmail>
2026-06-12 19:35:29 -0500

    fix: update

--- src/UbigeoProcessor.ts
+++ src/UbigeoProcessor.ts
@@ -9,28 +9,21 @@ export class UbigeoProcessor {
     public async run() {
         const districtPath = path.join(
             process.cwd(), 
-            'resources/ubigeo_peru_2016_distritos.json'
+            'resources/ubigeo_peru_2016_distritos.json' 
         );
         const departmentPath = path.join(
             process.cwd(), 
             'resources/ubigeo_peru_2016_departamentos.json'
         );
-        
         const provincesPath = path.join(
             process.cwd(),
-            'ubigeo_peru_2016_provincias.json'
+            'resources/ubigeo_peru_2016_provincias.json'
         );
 
         const [departmentsData, provincesData, districtData] = await Promise.all([
-            async () => {
-                return JSON.parse(await fs.readFile(departmentPath, 'utf-8'))
-            },
-            async () => {
-                return JSON.parse(await fs.readFile(provincesPath, 'utf-8'));
-            }, 
-            async () => {
-                return JSON.parse(await fs.readFile(provincesPath, 'utf-8'));
-            }
-        ])
+            fs.readFile(departmentPath, 'utf-8').then(JSON.parse),
+            fs.readFile(provincesPath, 'utf-8').then(JSON.parse),
+            fs.readFile(districtPath, 'utf-8').then(JSON.parse)
+        ]);
     }
 }
